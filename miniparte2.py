98a23e153123c0aba23a810a5d1001fc29d2071e
GuilleLink <32148569+GuilleLink@users.noreply.github.com>
2020-09-02 15:11:42 -0600

    Ejercicio 4

--- miniparte2.py
+++ miniparte2.py
@@ -2,11 +2,12 @@
 #Javier Capio - 17077
 #Guillermo Sandoval - 17577
 
-#Ejercicio1 El triangulo de Shierpinski
+#Ejercicio4 integral e^-x^2
 
 import random
 import math
 import matplotlib.pyplot as plt
+import numpy as np
 
 #Puntos donde empieza el fractal
 p1, p2, p3 = 0.20, 0.30, 0.50
@@ -14,29 +15,14 @@ p1, p2, p3 = 0.20, 0.30, 0.50
 it = 100000
 x, y = [],[]
 
-def transf(x, y, prob):
-    
-    if(prob <= p1):
-        x = x/2
-        y = y/2
-    elif(prob <= (p1+p2) and prob > p1):
-        x = x/2 + 0.5
-        y = y/2
-    elif(prob <= (p1+p2+p3) and prob > (p1+p2)):
-        x = x/2 + 0.25
-        y = y/2 + 0.5
-    return x, y
+#Puntos random generados
+y = np.random.rand(it)
+value = np.array([])
+for i in y:
+    value = np.append(value, [math.e**(-((1/i-1)**2))/(i**2)])
 
-pointx = random.random()
-pointy = random.random()
 
-for iteracion in range(0, it):
-    prob = random.random()
-    px, py = transf(pointx, pointy, prob)
-    x.append(px)
-    y.append(py)
-    pointx = px
-    pointy = py
+dentroFuncion = sum(value)
+proporcion = 2*(dentroFuncion/it)
 
-plt.scatter(x, y, s = 0.3, edgecolor = 'yellow')
-plt.show()
+print('La aproximacion es: ' + str(proporcion))
